df2cca8c7c8cb7f068ec65d0d9407130a9ce8035
KimGeantae <128386707+KimGeantae@users.noreply.github.com>
2024-11-14 16:26:46 +0900

    Update webcam_realsense_2.py

--- aruco_ws/src/webcam_realsense_2.py
+++ aruco_ws/src/webcam_realsense_2.py
@@ -19,6 +19,11 @@ qy_rot = math.sin(angle/2)
 qz_rot = 0
 qw_rot = math.cos(angle/2)
 
+angle_web = math.radians(90)  # 90도를 라디안으로 변환
+qx_rot_web = 0
+qy_rot_web = math.sin(angle_web/2)
+qz_rot_web = 0
+qw_rot_web = math.cos(angle_web/2)
 class DualArucoDetector(Node):
     def __init__(self):
         super().__init__('dual_aruco_detector')
@@ -31,7 +36,7 @@ class DualArucoDetector(Node):
         self.publisher_webcam = self.create_publisher(Image, 'aruco_image', 10)
         self.image_pub_webcam = self.create_publisher(String, 'webcam/image_processed', 10)
 
-        self.set_id = 3
+        self.set_id = 1
         self.subscription = self.create_subscription(Int32, 'set_id', self.listener_callback, 10)
 
         self.bridge = CvBridge()
@@ -49,7 +54,8 @@ class DualArucoDetector(Node):
         self.dist_realsense = np.array(intr.coeffs)
 
         # 웹캠 초기화
-        self.cap = cv2.VideoCapture(8)  # 기본 웹캠 사용
+        # self.cap = cv2.VideoCapture(8)  # 기본 웹캠 사용
+        self.cap = cv2.VideoCapture('/dev/video4')
         self.cap.set(cv2.CAP_PROP_FPS, 30)
         self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
         self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
@@ -128,10 +134,10 @@ class DualArucoDetector(Node):
                             pose_msg.pose.position.x = tvec[0][0] / 1000.0
                             pose_msg.pose.position.y = tvec[1][0] / 1000.0
                             pose_msg.pose.position.z = tvec[2][0] / 1000.0
-                            pose_msg.pose.orientation.w = qw * qw_rot - qx * qx_rot - qy * qy_rot - qz * qz_rot
-                            pose_msg.pose.orientation.x = qx * qw_rot + qw * qx_rot + qy * qz_rot - qz * qy_rot
-                            pose_msg.pose.orientation.y = qy * qw_rot + qw * qy_rot + qz * qx_rot - qx * qz_rot
-                            pose_msg.pose.orientation.z = qz * qw_rot + qw * qz_rot + qx * qy_rot - qy * qx_rot
+                            pose_msg.pose.orientation.w = qw * qw_rot_web - qx * qx_rot_web - qy * qy_rot_web - qz * qz_rot_web
+                            pose_msg.pose.orientation.x = qx * qw_rot + qw * qx_rot_web + qy * qz_rot_web - qz * qy_rot_web
+                            pose_msg.pose.orientation.y = qy * qw_rot + qw * qy_rot_web + qz * qx_rot_web - qx * qz_rot_web
+                            pose_msg.pose.orientation.z = qz * qw_rot + qw * qz_rot_web + qx * qy_rot_web - qy * qx_rot_web
                             
                             # tf2 브로드캐스트
                             t = TransformStamped()
@@ -154,26 +160,46 @@ class DualArucoDetector(Node):
 
 
                             self.pose_publisher.publish(pose_msg)
+
                 if frame_id == "base_link":
                     if marker_id[0] == self.set_id:
                         corner = corners[i].reshape((4, 2))
                         ret, rvec, tvec = cv2.solvePnP(self.marker_3d_edges, corner, cmtx, dist)
                         if ret:
-                            rotation_matrix = cv2.Rodrigues(rvec)[0]
-                            quat = transforms3d.quaternions.mat2quat(rotation_matrix)
-                            tvec[2][0] =0 #0 fix
+                            Ry_90 = np.array([[0, 0, 1],   
+                                                    [0, 1, 0],
+                                                [-1, 0, 0]])
+                            Rx_90 = np.array([[1, 0, 0],   
+                                        [0, 0, 1],
+                                        [0, -1, 0]])
+                            rotation_matrix, _ = cv2.Rodrigues(rvec)
+                            rotation_matrix = Rx_90 @ (Ry_90 @ rotation_matrix)
+                            rvec, _ = cv2.Rodrigues(rotation_matrix)
+                                    
+                            tvec = Rx_90 @ (Ry_90 @ tvec)
+                                    
+                            theta = math.atan2(rotation_matrix[1,0], rotation_matrix[0,0])
+                            theta_msg = Float32()
+                            theta_msg.data = theta
+                            self.theta_publisher.publish(theta_msg)
 
-                            # Pose 메시지 생성 및 퍼블리시
-                            pose_msg = PoseStamped()
-                            pose_msg.header.frame_id = frame_id
-                            pose_msg.header.stamp = self.get_clock().now().to_msg()
+
+                            quat = transforms3d.quaternions.mat2quat(rotation_matrix)
+                            tvec[2][0] =0
+                            qx, qy,qz,qw = quat[0], quat[1], quat[2], quat[3]
+                            pose_msg = PoseStamped()  # PoseStamped로 변경하여 헤더 포함
+                            pose_msg.header.frame_id = 'base_link'  # 헤더 프레임 설정
+                            pose_msg.header.stamp = self.get_clock().now().to_msg()  # 타임스탬프 설정
+
                             pose_msg.pose.position.x = tvec[0][0] / 1000.0
                             pose_msg.pose.position.y = tvec[1][0] / 1000.0
                             pose_msg.pose.position.z = tvec[2][0] / 1000.0
-                            pose_msg.pose.orientation.x = quat[1]
-                            pose_msg.pose.orientation.y = quat[2]
-                            pose_msg.pose.orientation.z = quat[3]
-                            pose_msg.pose.orientation.w = quat[0]
+                            pose_msg.pose.orientation.w = qw * qw_rot - qx * qx_rot - qy * qy_rot - qz * qz_rot
+                            pose_msg.pose.orientation.x = qx * qw_rot + qw * qx_rot + qy * qz_rot - qz * qy_rot
+                            pose_msg.pose.orientation.y = qy * qw_rot + qw * qy_rot + qz * qx_rot - qx * qz_rot
+                            pose_msg.pose.orientation.z = qz * qw_rot + qw * qz_rot + qx * qy_rot - qy * qx_rot
+
+                             
                                 
                             # tf2 브로드캐스트
                             t = TransformStamped()
